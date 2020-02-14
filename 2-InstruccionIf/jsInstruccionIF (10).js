391b2e87ca2020baf0c3e1e8e5e6192df27abe1a
Ignx <58445918+ignxal@users.noreply.github.com>
2020-02-14 09:29:32 -0300

    Ejercicios clase 4

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	var maximo = 10;
+	var minimo = 1;
+
+	numero = Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo);
+
+	if (numero == 9 || numero == 10) {
+		alert("EXCELENTE");
+	}
+	else if (numero >= 4) {
+		alert("APROBÓ");
+	}
+	else {
+		alert("Vamos, la próxima se puede");
+	}
+
 
 }//FIN DE LA FUNCIÓN
