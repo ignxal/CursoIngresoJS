7b29cff7089323f40d8596fd112f75b0766c5234
Ignx <58445918+ignxal@users.noreply.github.com>
2020-02-21 12:21:41 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -5,18 +5,21 @@ var num2;
 var resultado2;
 var resultado1;
 
-num1 = prompt("Ingrese numero 1");
-num2 = prompt("Ingrese numero 2");
-resultado2 = parseInt(num1 + num2);
-resultado1 = parseInt(num1 - num2);
+num1 = parseInt(prompt("Ingrese numero 1"));
+num2 = parseInt(prompt("Ingrese numero 2"));
+resultado2 = (num1 + num2);
+resultado1 = (num1 - num2);
 
 if (num1 > num2) {
     alert("La resta es " + resultado1);
 }
 else if (num1 < num2) {
-    alert("La suma es " + resultado2);
-    if (resultado2>10) 
+    if (resultado2>10) {
     alert("La suma es " + resultado2 + " y supero el 10");
+    }
+    else {
+    alert("La suma es " + resultado2); 
+    }
 }
 else {
     alert("El numero " + num1 + " y el " + num2 + " son iguales");
