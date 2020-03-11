0d6af5ebafd01c3536a84bb69e5b662dec5ca813
Ignx <ignacio.logiudice@gmail.com>
2020-03-11 15:18:33 -0300

    Ejercicio 8 for

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,24 @@
 function mostrar()
 {
+var numero;
+var conPrimo=0;
 
+numero = parseInt(prompt("Ingrese un numero"));
+while (isNaN(numero) || numero<1) {
+    numero = parseInt(prompt("Dato invalido. Ingrese un numero"));
+}
+for (var i = 0; i <= numero; i++) {
+    if (numero%i==0) {
+        console.log(i);
+        conPrimo++;
+    }
+}
 
-
+if (conPrimo == 2) {
+    alert("El numero es primo");
+}
+else {
+    alert("El numero no es primo");
+}
 
 }//FIN DE LA FUNCIÓN
